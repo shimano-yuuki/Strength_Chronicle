322ed30caefd1e96484c209fa4adc060fec7d583
serizawa-T <86996275+serizawa-T@users.noreply.github.com>
2023-05-28 02:23:45 +0900

    メモページ作成

--- lib/view/memo_page.dart
+++ lib/view/memo_page.dart
@@ -6,11 +6,55 @@ class MemoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Text('種目',style: TextStyle(fontSize: 24),),
+            MemoCardWidget(title: 'アームカール', color: Colors.deepOrange)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
       ),
     );
   }
 }
+
+class MemoCardWidget extends StatelessWidget {
+  const MemoCardWidget({Key? key, required this.title, required this.color}) : super(key: key);
+
+  final String title;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          SizedBox(
+            width: 300,
+            height: 50,
+            child: Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(width: 10,),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: color)
+                    ,),
+                  Text(title),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+  }
+}
+
