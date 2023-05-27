117b1ce709adf673d721338c2d2c17370d7f1b51
serizawa-T <86996275+serizawa-T@users.noreply.github.com>
2023-05-27 15:46:40 +0900

    必要なパッケージをインストール

---
+++ lib/view/memo_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MemoPage extends StatelessWidget {
+  const MemoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
