57e57f6c31a8b6d93fbc8be1cc84d7fef40d9331
serizawa-T <86996275+serizawa-T@users.noreply.github.com>
2023-06-05 11:37:12 +0900

    Revert "bottomSheetの作成とタブの編集"
    This reverts commit b3156a18e734dc68df3bf39a8200cd9cedee73da.

--- lib/view/memo_page.dart
+++ lib/view/memo_page.dart
@@ -17,7 +17,7 @@ class MemoPage extends StatelessWidget {
               children: [
                 const SizedBox(height: 24,),
                 const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Icon(Icons.cancel,color: Colors.grey,),
                     Text('種目を追加',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
@@ -39,12 +39,6 @@ class MemoPage extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(height: 24,),
-                /// ------------  部位選択  -------------///
-                DropdownButtonWidget(text: '部位を選択',),
-                const SizedBox(height: 24,),
-                DropdownButtonWidget(text: 'トレーニング種類',),
-                /// ------------------------ ///
                 const Center(
                 ),
               ],
@@ -63,9 +57,9 @@ class MemoPage extends StatelessWidget {
           appBar: const TabBar(
             // isScrollable: true,
               tabs: [
-                Tab(text: '全て',),
+                Tab(text: 'すべて',),
                 Tab(text: '胸',),
-                Tab(text: '背',),
+                Tab(text: '背中',),
                 Tab(text: '腕',),
                 Tab(text: '肩',),
                 Tab(text: '脚',),
@@ -146,51 +140,3 @@ class MemoCardWidget extends StatelessWidget {
   }
 }
 
-class DropdownButtonWidget extends StatelessWidget {
-  const DropdownButtonWidget({Key? key, required this.text}) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width:  10,
-                height: 10,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.orange,
-                ),
-              ),
-              SizedBox(width: 8,),
-              Text(text,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
-            ],
-          ),
-          DropdownButton(
-            hint: Text('選択してください'),
-            items: [
-              DropdownMenuItem(
-                child: Text('胸'),
-                value: '胸',
-              ),
-              DropdownMenuItem(
-                child: Text('背中'),
-                value: '背中',
-              ),
-              DropdownMenuItem(
-                child: Text('肩'),
-                value: '肩',
-              ),
-            ], onChanged: (String? value) {  },),
-        ],
-      ),
-    );
-  }
-}
