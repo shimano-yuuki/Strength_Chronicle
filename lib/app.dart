042f23f315ccd647f8aad4d75ba092f0bc5b0d0c
serizawa-T <86996275+serizawa-T@users.noreply.github.com>
2023-07-06 14:56:35 +0900

    bottomNavigationを修正

--- lib/app.dart
+++ lib/app.dart
@@ -1,38 +1,59 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:workout_app/view/calendar_page.dart';
 import 'package:workout_app/view/diary_page.dart';
 import 'package:workout_app/view/memo_page.dart';
 
-final _selectedIndexProvider = StateProvider((ref) => 0);
+part 'app.g.dart';
 
-class BottomNavigation extends ConsumerWidget{
+@riverpod
+class SelectedBottomNavigation extends _$SelectedBottomNavigation {
+  @override
+  int build() {
+    return 0;
+  }
+
+  void changeSelectedIndex(int index) {
+    state = index;
+  }
+}
+
+class BottomNavigation extends ConsumerWidget {
   const BottomNavigation({Key? key}) : super(key: key);
 
-  static final _screen = [MemoPage(),CalendarPage(),DiaryPage()];
-  static final _screenIcon = [Icons.edit_note,Icons.event,Icons.auto_stories,];
+  static final _screen = [MemoPage(), CalendarPage(), DiaryPage()];
+  static final _screenIcon = [
+    Icons.edit_note,
+    Icons.event,
+    Icons.auto_stories,
+  ];
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = ref.watch(selectedBottomNavigationProvider);
     return Scaffold(
-      body: _screen[ref.watch(_selectedIndexProvider)],
-      bottomNavigationBar:AnimatedBottomNavigationBar.builder(
+      body: _screen[selectedIndex],
+      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
         backgroundColor: Colors.white,
-          itemCount: _screenIcon.length,
-          activeIndex: ref.watch(_selectedIndexProvider),
+        itemCount: _screenIcon.length,
+        activeIndex: selectedIndex,
         gapLocation: GapLocation.none,
         splashColor: Colors.deepOrange,
-          onTap: (int index){
-            _screen[ref.read(_selectedIndexProvider.notifier).state = index];
-          }, tabBuilder: (int index, bool isActive) {
-        final color = isActive ? Colors.deepOrange : Colors.black;
-        return Icon(
-          _screenIcon[index],
-          size: 24,
-          color: color,
-        );
-      },
+        onTap: (int index) {
+          ref
+              .read(selectedBottomNavigationProvider.notifier)
+              .changeSelectedIndex(index);
+        },
+        tabBuilder: (int index, bool isActive) {
+          final color = isActive ? Colors.deepOrange : Colors.black;
+          return Icon(
+            _screenIcon[index],
+            size: 24,
+            color: color,
+          );
+        },
       ),
     );
   }
--- lib/view/memo_page.dart
+++ lib/view/memo_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:workout_app/app.dart';
 import 'package:workout_app/model/memo_state.dart';
 import 'package:workout_app/view/widget/bottom_sheet.dart';
 import 'package:workout_app/view/widget/tab.dart';
@@ -8,40 +9,145 @@ import 'package:workout_app/view_model/memo_notifier.dart';
 class MemoPage extends ConsumerWidget {
   const MemoPage({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
+    ListTile listTileWidget(String title, int index){
+      return ListTile(
+        title: Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          final selectedIndex  = ref.watch(selectedBottomNavigationProvider);
+          if (selectedIndex == index) {
+            Navigator.pop(context);
+          } else {
+            ref.read(selectedBottomNavigationProvider.notifier).changeSelectedIndex(index);
+          }
+        },
+      );
+    }
+
+    final appBar = AppBar(
+      title: Text(
+        '種目名',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.more_vert),
+          onPressed: () {
+            ref.read(visibleProvider.notifier).changeVisible(false);
+          },
+        ),
+      ],
+      centerTitle: true,
+      elevation: 5,
+      bottom: TabBar(
+        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+        tabs: [
+          Tab(text: '全て'),
+          Tab(text: '胸'),
+          Tab(text: '背中'),
+          Tab(text: '腕'),
+          Tab(text: '肩'),
+          Tab(text: '脚'),
+          Tab(text: '腹'),
+        ],
+      ),
+    );
+    final floatingActionButton = Stack(
+      children: [
+        Positioned(
+          bottom: 16.0,
+          right: 16.0,
+          child: DragTarget<Memo>(
+            builder: (context, candidateData, rejectedData) {
+              return GestureDetector(
+                onTap: () {
+                  // showDialog(
+                  //   context: context,
+                  //   barrierDismissible: true,
+                  //   builder: (_) {
+                  //     return AlertDialog(
+                  //       title: Text('確認'),
+                  //       content: Text('本当に削除しますか？'),
+                  //       actions: <Widget>[
+                  //         TextButton(
+                  //           child: Text('キャンセル'),
+                  //           onPressed: () => Navigator.of(context).pop(),
+                  //         ),
+                  //         TextButton(
+                  //           child: Text('削除'),
+                  //           onPressed: () {
+                  //             ref.read(memosProvider.notifier).removeMemo(data.id);
+                  //             Navigator.of(context).pop();
+                  //           },
+                  //         ),
+                  //       ],
+                  //     );
+                  //   },
+                  // );
+                },
+                child: Container(
+                  width: 56.0,
+                  height: 56.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        offset: Offset(0, 2),
+                        blurRadius: 4.0,
+                      ),
+                    ],
+                  ),
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            onAccept: (data) {
+              ref.read(memosProvider.notifier).removeMemo(data.id);
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 16.0,
+          right: 16.0,
+          child: Visibility(
+            visible: ref.watch(visibleProvider),
+            child: FloatingActionButton(
+              onPressed: () {
+                _showBottomSheetMenu(context, ref);
+              },
+              child: const Icon(Icons.add, color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
+    final drawer = Drawer(
+      child: ListView(
+        children: [
+          listTileWidget('メモページ',0),
+          listTileWidget('カレンダー',1),
+          listTileWidget('日記',2),
+        ],
+      ),
+    );
+
     return DefaultTabController(
       initialIndex: 0,
       length: 7,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '種目名',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () {
-                ref.read(visibleProvider.notifier).changeVisible(false);
-              },
-            ),
-          ],
-          centerTitle: true,
-          elevation: 5,
-          bottom: TabBar(
-            labelStyle: TextStyle(fontWeight: FontWeight.bold),
-            tabs: [
-              Tab(text: '全て'),
-              Tab(text: '胸'),
-              Tab(text: '背中'),
-              Tab(text: '腕'),
-              Tab(text: '肩'),
-              Tab(text: '脚'),
-              Tab(text: '腹'),
-            ],
-          ),
-        ),
+        appBar: appBar,
         body: TabBarView(
           children: [
             TabViewWidget(),
@@ -53,113 +159,8 @@ class MemoPage extends ConsumerWidget {
             TabViewWidget(part: '腹'),
           ],
         ),
-        floatingActionButton: Stack(
-          children: [
-            Positioned(
-              bottom: 16.0,
-              right: 16.0,
-              child: DragTarget<Memo>(
-                builder: (context, candidateData, rejectedData) {
-                  return GestureDetector(
-                    onTap: () {
-                      // showDialog(
-                      //   context: context,
-                      //   barrierDismissible: true,
-                      //   builder: (_) {
-                      //     return AlertDialog(
-                      //       title: Text('確認'),
-                      //       content: Text('本当に削除しますか？'),
-                      //       actions: <Widget>[
-                      //         TextButton(
-                      //           child: Text('キャンセル'),
-                      //           onPressed: () => Navigator.of(context).pop(),
-                      //         ),
-                      //         TextButton(
-                      //           child: Text('削除'),
-                      //           onPressed: () {
-                      //             ref.read(memosProvider.notifier).removeMemo(data.id);
-                      //             Navigator.of(context).pop();
-                      //           },
-                      //         ),
-                      //       ],
-                      //     );
-                      //   },
-                      // );
-                    },
-                    child: Container(
-                      width: 56.0,
-                      height: 56.0,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            offset: Offset(0, 2),
-                            blurRadius: 4.0,
-                          ),
-                        ],
-                      ),
-                      child: Icon(
-                        Icons.delete,
-                        color: Colors.white,
-                      ),
-                    ),
-                  );
-                },
-                onAccept: (data) {
-                  ref.read(memosProvider.notifier).removeMemo(data.id);
-                },
-              ),
-            ),
-            Positioned(
-              bottom: 16.0,
-              right: 16.0,
-              child: Visibility(
-                visible: ref.watch(visibleProvider),
-                child: FloatingActionButton(
-                  onPressed: () {
-                    _showBottomSheetMenu(context, ref);
-                  },
-                  child: const Icon(Icons.add, color: Colors.white),
-                ),
-              ),
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              ListTile(
-                title: Text(
-                  'メモページ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'カレンダー',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text(
-                  '日記',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        ),
+        floatingActionButton: floatingActionButton,
+        drawer: drawer,
       ),
     );
   }
