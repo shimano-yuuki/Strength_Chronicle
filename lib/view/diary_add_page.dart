546fa3df1843a679889086fb5b5fc7a7a6bb1050
simanoyuki <yuukireirei5@gmail.com>
2023-08-17 03:16:57 +0900

    追加画面を実装

--- lib/view/diary_add_page.dart
+++ lib/view/diary_add_page.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:workout_app/textstyle.dart';
 class DiaryAddPage extends ConsumerWidget {
   const DiaryAddPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    DateTime now = DateTime.now();
+    String formattedDate = "${now.month}月${now.day}日";
     return Scaffold(
         appBar: AppBar(
-        title: Text(
-        '日記を追加する',
-        style: TextStyle(fontWeight: FontWeight.bold),
-    ),
-    ),);
+          actions: [
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: GestureDetector(
+              child: Container(
+                height: 30,
+                width: 80,
+                decoration: BoxDecoration(color: Colors.orange,borderRadius: BorderRadius.circular(20)),
+                child: Center(child: Text('追加する',style: MyTextStyles.body.bold.white)),
+              ),
+              onTap: (){
+              },
+                ),
+            )
+          ],
+        ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+            crossAxisAlignment:CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40),
+              Text(formattedDate,style: MyTextStyles.title.large.bold),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 15,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: '今日の筋トレはどうでしたか？',
+                  ),
+                  autofocus: true,
+                )
+              ),
+        ]),
+      ),
+    );
   }
 }
