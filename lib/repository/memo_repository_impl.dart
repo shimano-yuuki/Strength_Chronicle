f7a507fd1f6d40f573e2ae8ab4171ee75ee75eeb
serizawa-T <86996275+serizawa-T@users.noreply.github.com>
2024-01-27 11:54:12 +0900

    add: メモを作成した際にfirebaseにデータを保存できるように変更

--- lib/repository/memo_repository_impl.dart
+++ lib/repository/memo_repository_impl.dart
@@ -1,10 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:workout_app/model/auth/firebase_auth.dart';
 import 'package:workout_app/model/memo/memo_state.dart';
 import 'package:workout_app/repository/memo_repository.dart';
 
-class MemoRepositoryImpl implements MemoRepository{
+class MemoRepositoryImpl implements MemoRepository {
+  @override
+  User? get currentUser => AuthService().currentUser;
+
   @override
   Future<void> createMemo({required Memo memo}) async {
-
+    final uid = currentUser!.uid;
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .collection('memos')
+        .add(Memo(
+                id: memo.id,
+                title: memo.title,
+                part: memo.part,
+                type: memo.type)
+            .toJson());
   }
 
+  // @override
+  // Future<void> deleteMemo({required Memo memo}) async{
+  //   final uid = currentUser!.uid;
+  //   FirebaseFirestore.instance.collection('users').doc(uid).collection('memos').doc()
+  // }
 }
